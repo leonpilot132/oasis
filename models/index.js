513d09c72c374af8b14d8bd91a5fc5e72701a8f9
Leon Infant. B <leonbas79@gmail.com>
2025-06-27 11:50:35 +0530

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,12 @@
+// models/index.js
+const User = require('./User');
+const MenuItem = require('./MenuItem');
+const CustomPizza = require('./CustomPizza');
+const Order = require('./Order');
+
+module.exports = {
+    User,
+    MenuItem,
+    CustomPizza,
+    Order
+};
